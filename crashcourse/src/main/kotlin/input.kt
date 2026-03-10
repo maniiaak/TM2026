b5326c9715887aba2d37df77bbce6a695b6e1e6b
maniiaak <github.afford868@passmail.net>
2026-03-10 11:10:25 +0100

    reverse

--- crashcourse/src/main/kotlin/input.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-    print("Please enter a number: ")
-    val input = readln()
-    val inputAsInteger = input.toInt()
-
-    val isEven = inputAsInteger % 2 == 0
-    println("Is the number even? $isEven")
-}
