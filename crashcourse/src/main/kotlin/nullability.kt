8f0da1dacf9de816737d3fcbff799f63d1115933
maniiaak <github.afford868@passmail.net>
2026-03-10 11:10:25 +0100

    reverse

--- crashcourse/src/main/kotlin/nullability.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    print("Please enter a number: ")
-    val input = readln()
-    val inputAsInteger = input.toIntOrNull()?.rem(2)?.equals(0)
-
-    println("Is even? $inputAsInteger")
-}
