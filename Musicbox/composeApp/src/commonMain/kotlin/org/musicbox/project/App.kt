3ad4026cbfaf5dcad1c566867aaf66e866dc8ad3
maniiaak <github.afford868@passmail.net>
2026-03-14 17:16:24 +0100

    Welcome message changed

--- Musicbox/composeApp/src/commonMain/kotlin/org/musicbox/project/App.kt
+++ Musicbox/composeApp/src/commonMain/kotlin/org/musicbox/project/App.kt
@@ -41,7 +41,7 @@ fun App() {
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
                     Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+                    Text("Compose: Musicbox user :)")
                 }
             }
         }
