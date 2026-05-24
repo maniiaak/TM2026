bec49dbe8be8dca091fa7af0d5e3a94ab4da5876
maniiaak <github.afford868@passmail.net>
2026-05-24 15:47:48 +0200

    Les reviews sont visibles sur la page de l'album

--- KMP-App-Template-main/composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/data/MuseumObject.kt
+++ KMP-App-Template-main/composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/data/MuseumObject.kt
@@ -3,16 +3,26 @@ package com.jetbrains.kmpapp.data
 import kotlinx.serialization.Serializable
 
 @Serializable
+data class Review(
+    val userID: Int,
+    val username: String,
+    val content: String,
+    val createdAt: String,
+    val rating: Double
+)
+
+@Serializable
 data class MuseumObject(
     val objectID: Int,
     val title: String,
     val artistDisplayName: String,
-    val type: String,
-    val objectURL: String,
     val objectDate: String,
-    val coverImage: String,
-    val tracks: String,
+    val type: String,
     val length: String,
-    val totalRatings: Int,
-    val rating: Float,
+    val tracks: String,
+    val coverImage: String?,
+    val objectURL: String?,
+    val rating: Double, // Average rating
+    val totalRatings: Int, // Total count
+    val reviews: List<Review>? // The list of reviews
 )
