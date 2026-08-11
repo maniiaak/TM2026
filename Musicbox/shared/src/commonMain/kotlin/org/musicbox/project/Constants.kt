96d17bfba2a2c7dab6ea064aa1d00297d469cbed
maniiaak <github.afford868@passmail.net>
2026-08-11 23:17:39 +0200

    -updated readme.md -used Docker to create and manage the dev and the prod server -deleted useless files

--- Musicbox/shared/src/commonMain/kotlin/org/musicbox/project/Constants.kt
+++
@@ -1,3 +0,0 @@
-package org.musicbox.project
-
-const val SERVER_PORT = 8080
--- Musicbox/shared/src/commonMain/kotlin/org/musicbox/project/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package org.musicbox.project
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
