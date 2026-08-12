5c08712be65704c5b93b315f63ffdfa6fe7044e0
maniiaak <github.afford868@passmail.net>
2026-08-12 15:41:21 +0200

    fix: -new package name
    bugs:
    -spotify auth doesn't work: redirect url wrong (should be fixed with firebase auth)

--- iluvmusic-app/composeApp/src/commonMain/kotlin/com/jetbrains/com/maniiaak/iluvmusic/config/ApiConfig.kt
+++
@@ -1,6 +0,0 @@
-package com.maniiaak.iluvmusic.config
-
-object ApiConfig {
-    // Change to your server's IP adress
-    const val BASE_URL = "http://192.168.1.139:5000/api"
-}
--- iluvmusic-app/composeApp/src/commonMain/kotlin/com/jetbrains/com/maniiaak/iluvmusic/utils/PlatformTime.kt
+++
@@ -1,3 +0,0 @@
-package com.maniiaak.iluvmusic.utils
-
-expect fun getCurrentTimestamp(): Long
