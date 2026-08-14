518395cb5bf9277e700ddc0a3579ee8b30528041
maniiaak <github.afford868@passmail.net>
2026-08-14 18:38:52 +0200

    login system done and stable

--- iluvmusic-app/composeApp/src/commonMain/kotlin/com/maniiaak/iluvmusic/data/MuseumApi.kt
+++ iluvmusic-app/composeApp/src/commonMain/kotlin/com/maniiaak/iluvmusic/data/MuseumApi.kt
@@ -5,6 +5,7 @@ import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 interface MuseumApi {
