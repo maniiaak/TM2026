1dcbfbd708db1dcab77ac6f0e469b2f4d964f1f8
maniiaak <github.afford868@passmail.net>
2026-08-17 23:50:26 +0200

    fix: ui looks a lot nicer

--- iluvmusic-app/composeApp/src/wasmJsMain/kotlin/com/maniiaak/iluvmusic/Main.kt
+++ iluvmusic-app/composeApp/src/wasmJsMain/kotlin/com/maniiaak/iluvmusic/Main.kt
@@ -1,15 +1,26 @@
-package com.maniiaak.iluvmusic
-
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.window.ComposeViewport
+import com.maniiaak.iluvmusic.App
 import com.maniiaak.iluvmusic.di.initKoin
+import kotlinx.browser.window
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     try {
         initKoin()
+        val screenWidth = window.innerWidth
+        val scaleFactor = when {
+            screenWidth < 600 -> 1f   // narrow/mobile browser
+            screenWidth < 1200 -> 1.5f    // typical laptop
+            else -> 2f                // large desktop monitor
+        }
         ComposeViewport("composeApp") {
-            App()
+            CompositionLocalProvider(LocalDensity provides Density(density = scaleFactor)) {
+                App()
+            }
         }
     } catch (e: Throwable) {
         println("Startup failure: ${e.message}")
